a3e0d7263007d535043fb9be17aa2a08e34f0039
Hiroko Nakano <1327024+nakanohiroko@users.noreply.github.com>
2023-08-21 22:09:45 +0900

    favicon設定、GA導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,17 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://daily.corleonis.net/',
-  integrations: [mdx(), sitemap(), svelte(), tailwind()]
+  integrations: [mdx(), sitemap(), svelte(), tailwind(),partytown({
+    // Adds dataLayer.push as a forwarding-event.
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  }),
+  ],
 });
