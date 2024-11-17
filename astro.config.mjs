b7a4657812417e57c62492e8df54ad975143eb07
nakanohiroko <piroconbu@gmail.com>
2024-11-17 17:38:39 +0900

    [add] 画面プレビューページ（SSR）を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,22 @@ import sitemap from '@astrojs/sitemap';
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
+import node from '@astrojs/node'; // Node.js SSR アダプターを追加
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://daily.corleonis.net/',
-  integrations: [mdx(), sitemap(), svelte(), tailwind(),partytown({
-    // Adds dataLayer.push as a forwarding-event.
-    config: {
-      forward: ["dataLayer.push"],
-    },
-  }),
+  output: 'hybrid', // ハイブリッドモードに変更 (SSG + SSR)
+  integrations: [
+    mdx(),
+    sitemap(),
+    svelte(),
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
   ],
+  adapter: node({ mode: 'standalone' }), // Node.js用アダプター
 });
